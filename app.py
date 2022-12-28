4960ea89efd469c667ac3802da00b762e6f7a644
Imoh <ukben008@gmail.com>
2022-12-28 20:54:06 +0100

    app.py b

--- app.py
+++ app.py
@@ -3,9 +3,6 @@
 import streamlit as st
 import pandas as pd
 
-import os
-print(os.getcwd())
-
 path = "C://Users//USER//Desktop//Computer Practice//Excel Web Application"
 file_name = "World-Happiness-Report-2022.csv"
 df = pd.read_csv(f"{path}{file_name}")
